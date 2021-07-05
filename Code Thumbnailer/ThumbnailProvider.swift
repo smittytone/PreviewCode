06a56448ab6e9c86d4c0557f710ae9d6137c495d
smittytone <smittytone@icloud.com>
2021-07-05 09:09:37 +0100

    sync

--- Code Thumbnailer/ThumbnailProvider.swift
+++ Code Thumbnailer/ThumbnailProvider.swift
@@ -12,45 +12,45 @@ import Cocoa
 
 
 class ThumbnailProvider: QLThumbnailProvider {
-    
+
     // MARK:- Private Properties
-    
+
     private var appSuiteName: String = MNU_SECRETS.PID + BUFFOON_CONSTANTS.SUITE_NAME
-    
-    
+
+
     // MARK:- Lifecycle Required Functions
-    
+
     override init() {
-        
-        /* 
+
+        /*
          * Override the init() method so we can do all the setup we need
          * BEFORE we start rendering, ie. so we don't write values another
          * thread may be trying to read
          */
-        
+
         // Must call the super class because we don't know
         // what operations it performs
         super.init()
-        
+
         // Set the base values once per instantiation, not every
         // time a string is rendered (which risks a race condition)
         setBaseValues(true)
     }
 
-    
+
     override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
-        
+
         /*
          * This is the main entry point for macOS' thumbnailing system
          */
-        
+
         let targetHeight: CGFloat = request.maximumSize.height
         let targetWidth: CGFloat = CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ASPECT) * request.maximumSize.height
         let thumbnailFrame: CGRect = NSMakeRect(0.0,
                                                 0.0,
                                                 targetWidth,
                                                 targetHeight)
-        
+
         handler(QLThumbnailReply.init(contextSize: thumbnailFrame.size) { () -> Bool in
             // Place all the remaining code within the closure passed to 'handler()'
             //let success = autoreleasepool { () -> Bool in
@@ -63,13 +63,13 @@ class ThumbnailProvider: QLThumbnailProvider {
                         // as we're not going to read it again any time soon
                         let data: Data = try Data.init(contentsOf: request.fileURL, options: [.uncached])
                         guard let codeFileString: String = String.init(data: data, encoding: .utf8) else { return false }
-                        
+
                         // Set the language
                         let language: String = getLanguage(request.fileURL.path, false)
-                        
+
                         // Get the Attributed String
                         let codeAttString: NSAttributedString = getAttributedString(codeFileString, language, true)
-                        
+
 
                         // Set the primary drawing frame and a base font size
                         let codeFrame: CGRect = CGRect.init(x: BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ORIGIN_X,
@@ -80,19 +80,19 @@ class ThumbnailProvider: QLThumbnailProvider {
                         // Instantiate an NSTextField to display the NSAttributedString render of the code
                         let codeTextField: NSTextField = NSTextField.init(labelWithAttributedString: codeAttString)
                         codeTextField.frame = codeFrame
-                        
+
                         // Also generate text for the bottom-of-thumbnail file type tag
                         // Define the frame of the tag area
                         let tagFrame: CGRect = CGRect.init(x: BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ORIGIN_X,
                                                            y: BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ORIGIN_Y,
                                                            width: BUFFOON_CONSTANTS.THUMBNAIL_SIZE.WIDTH,
                                                            height: BUFFOON_CONSTANTS.THUMBNAIL_SIZE.TAG_HEIGHT)
-                        
+
                         // Set the paragraph style we'll use -- just centred text
                         let style: NSMutableParagraphStyle = NSMutableParagraphStyle.init()
                         style.alignment = .center
                         style.lineBreakMode = .byTruncatingMiddle
-                        
+
                         // Set the point size
                         let tag: String = getLanguage(request.fileURL.path, true).uppercased()
                         var fontSize: CGFloat = CGFloat(BUFFOON_CONSTANTS.TAG_TEXT_SIZE)
@@ -104,7 +104,7 @@ class ThumbnailProvider: QLThumbnailProvider {
                                 fontSize = CGFloat(BUFFOON_CONSTANTS.TAG_TEXT_MIN_SIZE)
                             }
                         }
-                        
+
                         // Build the tag's string attributes
                         let tagAtts: [NSAttributedString.Key: Any] = [
                             .paragraphStyle: style as NSParagraphStyle,
@@ -115,10 +115,10 @@ class ThumbnailProvider: QLThumbnailProvider {
                         // Instantiate an NSTextField to display the NSAttributedString render of the tag
                         let tagTextField: NSTextField = NSTextField.init(labelWithAttributedString: NSAttributedString.init(string: tag, attributes: tagAtts))
                         tagTextField.frame = tagFrame
-                        
-                        // Generate the bitmap from the rendered YAML text view
+
+                        // Generate the bitmap from the rendered code text view
                         guard let imageRep: NSBitmapImageRep = codeTextField.bitmapImageRepForCachingDisplay(in: codeFrame) else { return false }
-                        
+
                         // Draw the code view into the bitmap and then the tag
                         codeTextField.cacheDisplay(in: codeFrame, to: imageRep)
                         tagTextField.cacheDisplay(in: tagFrame, to: imageRep)
@@ -137,19 +137,19 @@ class ThumbnailProvider: QLThumbnailProvider {
             //return success
         }, nil)
     }
-    
-    
+
+
     /*
     // MARK:- Utility Functions
-    
+
     /**
      Create an attributed string for a file icon tag.
-     
+
      We'll use it to determine the file's programming language.
-     
+
      - Parameters:
         - tag:   The text of the tag.
-     
+
      - Returns: The tag as an NSAttributedString.
      */
     func getTagString(_ tag: String) -> NSAttributedString {
@@ -158,7 +158,7 @@ class ThumbnailProvider: QLThumbnailProvider {
         let style: NSMutableParagraphStyle = NSMutableParagraphStyle.init()
         style.alignment = .center
         style.lineBreakMode = .byTruncatingMiddle
-        
+
         // Set the point size
         var fontSize: CGFloat = CGFloat(BUFFOON_CONSTANTS.TAG_TEXT_SIZE)
         let renderSize: NSSize = (tag as NSString).size(withAttributes: [.font: NSFont.systemFont(ofSize: fontSize)])
@@ -169,7 +169,7 @@ class ThumbnailProvider: QLThumbnailProvider {
                 fontSize = CGFloat(BUFFOON_CONSTANTS.TAG_TEXT_MIN_SIZE)
             }
         }
-        
+
         // Build the tag's string attributes
         let tagAtts: [NSAttributedString.Key: Any] = [
             .paragraphStyle: style,
