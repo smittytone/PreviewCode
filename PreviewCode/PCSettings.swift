f722786943a4e436de41b19ebb482a4bbc4dcf66
Tony Smith (smittytone) <tony@smittytone.net>
2025-08-30 15:54:10 +0100

    Finalise savablae settings list

--- PreviewCode/PCSettings.swift
+++ PreviewCode/PCSettings.swift
@@ -16,35 +16,35 @@ import Foundation
 
 class PCSettings {
 
-    // Saved Settings
-    var fontName: String                    = BUFFOON_CONSTANTS.DEFAULT_FONT
-    var fontSize: CGFloat                   = CGFloat(BUFFOON_CONSTANTS.BASE_PREVIEW_FONT_SIZE)
-    var lineSpacing: CGFloat                = BUFFOON_CONSTANTS.DEFAULT_LINE_SPACING
-    var darkThemeName: String               = BUFFOON_CONSTANTS.DEFAULT_THEME_DARK
-    var lightThemeName: String              = BUFFOON_CONSTANTS.DEFAULT_THEME_LIGHT
+    // MARK: Properties
+    var fontName: String                    = BUFFOON_CONSTANTS.DEFAULTS.FONT
+    var fontSize: CGFloat                   = CGFloat(BUFFOON_CONSTANTS.DEFAULTS.FONT_SIZE)
+    var lineSpacing: CGFloat                = BUFFOON_CONSTANTS.DEFAULTS.LINE_SPACING
+    var darkThemeName: String               = BUFFOON_CONSTANTS.DEFAULTS.DARK_THEME
+    var lightThemeName: String              = BUFFOON_CONSTANTS.DEFAULTS.LIGHT_THEME
     var themeDisplayMode: Int               = BUFFOON_CONSTANTS.DISPLAY_MODE.AUTO
-    // FROM 2.0.0
-    var doShowMargin: Bool                  = false
     var doShowLineNumbers: Bool             = false
 
-    // Unsaved Settings
-    var lightThemeIndex: Int                = 0
-    var darkThemeIndex: Int                 = 0
+
+    // MARK: Retrieve and Persist Functions
 
     /**
      Populate the current settings value with those read from disk.
+
+     - Parameters:
+        - suite The App Suite name under which the defaults are recorded.
      */
     func loadSettings(_ suite: String) {
         
         // The suite name is the app group name, set in each extension's entitlements, and the host app's
         if let defaults = UserDefaults(suiteName: suite) {
-            self.fontName = defaults.string(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_FONT_NAME) ?? BUFFOON_CONSTANTS.DEFAULT_FONT
+            self.fontName = defaults.string(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_FONT_NAME) ?? BUFFOON_CONSTANTS.DEFAULTS.FONT
             self.fontSize = CGFloat(defaults.float(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_FONT_SIZE))
             self.lineSpacing = CGFloat(defaults.float(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_LINE_SPACING))
-            self.darkThemeName = defaults.string(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_DARK_NAME) ?? BUFFOON_CONSTANTS.DEFAULT_THEME_DARK
-            self.lightThemeName = defaults.string(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_LIGHT_NAME) ?? BUFFOON_CONSTANTS.DEFAULT_THEME_LIGHT
+            self.darkThemeName = defaults.string(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_DARK_NAME) ?? BUFFOON_CONSTANTS.DEFAULTS.DARK_THEME
+            self.lightThemeName = defaults.string(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_LIGHT_NAME) ?? BUFFOON_CONSTANTS.DEFAULTS.LIGHT_THEME
             self.themeDisplayMode = defaults.integer(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_THEME_MODE)
-            //self.doShowMargin = defaults.bool(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_SHOW_MARGIN)
+            self.doShowLineNumbers = defaults.bool(forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_SHOW_LINE_NUMBERS)
         }
     }
 
@@ -52,6 +52,9 @@ class PCSettings {
     /**
      Write Settings page state values to disk, but only those that have been changed.
      If this happens, also update the current settings store
+
+     - Parameters:
+        - suite The App Suite name under which the defaults are recorded.
      */
     func saveSettings(_ suite: String) {
         
@@ -63,7 +66,7 @@ class PCSettings {
             defaults.setValue(self.darkThemeName, forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_DARK_NAME)
             defaults.setValue(self.lightThemeName, forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_LIGHT_NAME)
             defaults.setValue(self.themeDisplayMode, forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_THEME_MODE)
-            //defaults.setValue(self.doShowMargin, forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_SHOW_MARGIN)
+            defaults.setValue(self.doShowLineNumbers, forKey: BUFFOON_CONSTANTS.PREFS_IDS.PREVIEW_SHOW_LINE_NUMBERS)
         }
     }
 }
